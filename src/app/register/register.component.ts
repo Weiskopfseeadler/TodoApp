ac2382e8c43647621b8ce00c7831b84ee8c0a8ab
Weiskopfseeadler <patrick.grunder@zapp.ch>
2019-03-15 09:14:58 +0100

    final

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {UserService} from '../user.service';
 import {User} from "../Models/user";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -11,13 +12,18 @@ export class RegisterComponent implements OnInit {
 
   @Input() user:User=new  User();
 
-  constructor(private UserService: UserService) { }
+  constructor(private UserService: UserService,private  Router : Router) { }
 
   ngOnInit() {
   }
 
   add(){
-    this.UserService.addUser(this.user).subscribe((user)=>console.log(user));
+    this.UserService.addUser(this.user).subscribe((user)=>
+    {
+      console.log(user);
+      localStorage.setItem("user", JSON.stringify(user));
+      this.Router.navigateByUrl("/show-todo");
+    });
   }
 
 }
